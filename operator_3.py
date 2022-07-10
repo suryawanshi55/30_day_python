d31acdd6bf8a74281ecf521d0de7c4f97da40708
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-10 14:48:20 +0530

    Add files via upload

---
+++ operator_3.py
@@ -0,0 +1,45 @@
+# x=5
+# print(type(x)is int)             #True
+# print(type(x)is float)          # False
+# print(type(x)is not float)        #True
+# y=32.5
+# print(type(y)is int)                 #false
+# print(type(y)is float )            #  true
+# print(type(y)is not float)           #false
+
+# ....membership operator....
+# Name=["pooja",'mona','sujata','anita','shiva','leena','avi']
+# print('thilo' in Name)         # False
+# print('pooja' in Name)          #True
+# print('suraj' in Name)          #False
+# print('avi' in Name)            #True
+# print('Anita' in Name)          #False
+# print('Mona' in Name)           #False
+# Name .sort()
+# print(Name)        # ['anita', 'avi', 'leena', 'mona', 'pooja', 'shiva', 'sujata']
+# name=['k','l','r','a','d','g']
+# name.sort()
+# print(*name)      a d g k l r
+
+# for i in name:
+#     print(i)
+#     k
+#     l
+#     r
+#     a
+#     d
+#     g
+
+
+# .......shift operator......(>>Right/2 & <<left*2)
+# a=20
+# print(a>>1)           #10
+# print(a>>2)           #5
+# print(a>>3)           #2
+# print(a>>4)           #1
+
+# v=16
+# print(v<<1)   #32
+# print(v<<2)   #64
+# print(v<<3)   #128
+# print(v<<4)   #256
